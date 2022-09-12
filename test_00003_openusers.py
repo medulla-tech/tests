0e28bd5637d40b3141c79aeaccdda2f113c52533
Nicolas Lécureuil <neoclust@mageia.org>
2022-09-12 13:57:12 +0200

    Convert test 3 to be used under pytest

---
+++ test_00003_openusers.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import  expect, Page
+
+def test_open_users(page: Page) -> None:
+
+    page.goto('http://dev.siveo.net')
+
+    # We fille username/password and we connect into the mmc.
+    page.fill('#username', 'root')
+    page.fill('#password', 'siveo')
+    page.click('#connect_button')
+    expect(page).to_have_url("http://dev.siveo.net/mmc/main.php?module=base&submod=main&action=default")
+
+    page.click('#navbarusers')
+    expect(page).to_have_url("http://dev.siveo.net/mmc/main.php?module=base&submod=users&action=index")
+
