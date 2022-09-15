8954f2f5370aae01bc7f85cb5ce8d5ec2bce015d
Nicolas Lécureuil <neoclust@mageia.org>
2022-09-15 12:24:09 +0200

    Add tests to open every page from the imaging sidebar

---
+++ test_00014_open_imaging_manage_bootmenu.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import  expect, Page
+
+def test_open_imaging_manage_bootmenu(page: Page) -> None:
+
+    page.goto('http://dev.siveo.net')
+
+    # We fille username/password and we connect into the mmc.
+    page.fill('#username', 'root')
+    page.fill('#password', 'siveo')
+    page.click('#connect_button')
+    expect(page).to_have_url("http://dev.siveo.net/mmc/main.php?module=base&submod=main&action=default")
+
+    page.click('#navbarmanage')
+    expect(page).to_have_url("http://dev.siveo.net/mmc/main.php?module=imaging&submod=manage&action=index")
+
+    page.locator("//html/body/div/div[4]/div/div[1]/ul/li[4]/a").click()
+    # As the page can have issues, with wrong php include , or wrong session
+    # we wait a second to be sure we have the final page.
+    time.sleep(1)
+    expect(page).to_have_url("http://dev.siveo.net/mmc/main.php?module=imaging&submod=manage&action=bootmenu")
