054eb6e9e9ab6af4c2bde937de507e80f3d0b861
Nicolas Lécureuil <neoclust@mageia.org>
2022-09-22 12:04:31 +0200

    (tests) Migrate all user tests into test_00003_users.py

--- test_00003_users.py
+++ test_00003_users.py
@@ -11,6 +11,19 @@ test_server = Config.get('test_server', 'name')
 login = Config.get('test_server', 'login')
 password = Config.get('test_server', 'password')
 
+def test_open_users(page: Page) -> None:
+
+    page.goto(test_server)
+
+    # We fill username/password and we connect into the mmc.
+    page.fill('#username', login)
+    page.fill('#password', password)
+    page.click('#connect_button')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=main&action=default")
+
+    page.click('#navbarusers')
+    expect(page).to_have_url( test_server + "/mmc/main.php?module=base&submod=users&action=index")
+
 def test_create_users(page: Page) -> None:
 
     page.goto(test_server)
@@ -36,3 +49,24 @@ def test_create_users(page: Page) -> None:
 
     page.click(".btnPrimary[type='submit']")
     expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=users&action=edit&user=test_user")
+
+
+def test_delete_users(page: Page) -> None:
+
+    page.goto(test_server)
+
+    # We fill username/password and we connect into the mmc.
+    page.fill('#username', login)
+    page.fill('#password', password)
+    #page.click('#connect_button')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=main&action=default")
+
+    page.click('#navbarusers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=users&action=index")
+    page.click('//html/body/div/div[4]/div/div[2]/form/table/tbody/tr[2]/td[5]/ul/li[3]/a')
+    page.click('#delfiles')
+    page.click(".btnPrimary[type='submit']")
+
+    page.click(".btnPrimary[type='submit']")
+    time.sleep(1)
+    expect(pagei).to_have_url(test_server + "/mmc/main.php?module=base&submod=users&action=index")
