bb5a694b65b3538a3e87537886fc86e74086cc83
Nicolas Lécureuil <neoclust@mageia.org>
2022-12-12 10:44:20 +0100

    (tests computers) Remove some already done todo'

--- test_00002_computers.py
+++ test_00002_computers.py
@@ -18,9 +18,6 @@ password = Config.get('test_server', 'password')
     The tests are done to test the computer page of pulse.
 
     Test to be done:
-    -> Open the computer page.
-    -> Open all left pages
-    -> Open inventory of a machine
     -> Open all actions of a machine
     -> Do a search
     -> List online/offline computers.
