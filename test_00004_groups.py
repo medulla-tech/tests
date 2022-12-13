3b1b1d7188101a54b9b65c6a5f54a3bdde986ebb
Nicolas Lécureuil <neoclust@mageia.org>
2022-12-13 23:10:26 +0100

    (tests computer groups) Add all possible groups ( except one ). Add all possible groups except online/offline groups ( will be done in a commit later).

--- test_00004_groups.py
+++ test_00004_groups.py
@@ -171,6 +171,216 @@ def test_create_group_based_on_machine_type(page: Page) -> None:
     expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
 
 
+def test_create_group_based_on_machine_manufacturer(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="System-manufacturer"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Machine Manufacturer")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_machine_model(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="System-model"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Machine Model")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_machine_owner(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Owner-of-the-machine"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Machine Owner")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_last_logged_user(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Last-Logged-User"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Last Logged User")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_user_location(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="User-location"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By User location")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_location(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Location"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Location")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_state(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="State"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By State")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_entity(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Entity"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Entity")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_operating_system(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Operating-system"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Operating System")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_installed_software(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Installed-software"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Installed Software")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
 def test_create_group_based_on_software_name_and_version(page: Page) -> None:
 
     medulla_connect(page)
@@ -192,6 +402,93 @@ def test_create_group_based_on_software_name_and_version(page: Page) -> None:
     page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Software Name and Version")
     page.click(".btnPrimary[type='submit']")
 
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_os_version(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="OS-Version"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By OS Version")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_architecture(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Architecture"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Architecture")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_register_key(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Register-key"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Register Key")
+    page.click(".btnPrimary[type='submit']")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
+
+def test_create_group_based_on_register_key_value(page: Page) -> None:
+
+    medulla_connect(page)
+
+    page.click('#navbarcomputers')
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=machinesList")
+
+    page.click("#computersgroupcreator")
+    expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=computers&action=computersgroupcreator")
+
+    page.locator('#glpi').click()
+    page.locator('//*[@id="Register-key-value"]').click()
+    page.locator('//*[@id="autocomplete"]').click()
+    page.locator('//*[@id="autocomplete"]').fill("*win*")
+    page.click(".btnPrimary[type='button']")
+    page.locator('//*[@id="autocomplete2"]').click()
+    page.locator('//*[@id="autocomplete2"]').fill("*win*")
+    page.click(".btnPrimary[type='submit']")
+    page.click(".btnPrimary[type='button']")
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").click()
+    page.locator("//html/body/div/div[4]/div/table[2]/tbody/tr[1]/td[1]/input").fill("Group Created by playwright By Register Key value")
+    page.click(".btnPrimary[type='submit']")
     expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
 
 def test_create_group_based_by_OU_User(page: Page) -> None:
