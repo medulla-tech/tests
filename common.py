8fc651796966596b1ef751fb1c3f42c4f5d2568a
Nicolas Lécureuil <neoclust@mageia.org>
2022-12-16 16:02:17 +0100

    (test common) Add a sqlcheck function
    Add a function to query the SQL.
    This will allow us to see if the query fits with the template of what we
    are intended to have.

--- common.py
+++ common.py
@@ -2,12 +2,22 @@ from playwright.sync_api import  expect, Page
 
 import configparser
 import os
+import MySQLdb
+import sys
+
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 Config = configparser.ConfigParser()
 Config.read(os.path.join(project_dir, "config.ini"))
 
 test_server = Config.get('test_server', 'name')
+
+hostname = Config.get('test_server', 'ssh')
+user = Config.get('test_server', 'login')
+password = Config.get('test_server', 'password')
+port = Config.get('test_server', 'port')
+mysqluser = Config.get('test_server', 'mysqluser')
+mysqlpass = Config.get('test_server', 'mysqlpass')
 
 def medulla_connect(page: Page) -> None:
 
@@ -19,3 +29,28 @@ def medulla_connect(page: Page) -> None:
     page.click('#connect_button')
 
     expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=main&action=default")
+
+
+
+def sqlcheck(base, sql_request):
+    """
+        Used to run SQL requests
+        Args:
+            db: The database where we will do the requests.
+            sql_request: The SQL Request that  will be played
+    """
+    try:
+        db = MySQLdb.connect(host=hostname,
+                            user=mysqluser,
+                            passwd=mysqlpass,
+                            port=int(port),
+                            db=base)
+
+        cursor = db.cursor()
+        record = cursor.execute(sql_request)
+
+        for row in cursor.fetchone():
+            return row
+
+    except Exception as e:
+        sys.exit(1)
