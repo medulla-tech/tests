994146f97b47e145597fc5c086ed49ff4c3eac70
Nicolas Lécureuil <neoclust@mageia.org>
2022-12-16 17:24:01 +0100

    ( test computer groups) Remove unneeded debug

--- test_00005_computers_groups.py
+++ test_00005_computers_groups.py
@@ -46,8 +46,6 @@ def test_create_group_based_on_name(page: Page) -> None:
     expect(page).to_have_url(re.compile(".*submod=computers&action=save_detail*"))
 
     result_on_name = sqlcheck("dyngroup", "SELECT query FROM Groups WHERE name = 'Group Created by playwright By Name'")
-    mylogger.info(' record %s '% result_on_name)
-
 
     normal_result = "1==glpi::Computer name==*win*"
 
