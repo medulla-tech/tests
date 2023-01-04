cac7b3777b499abc3628fd399eaf0313c01aead0
Adrien T <a.thaissen@gmail.com>
2023-01-04 15:35:39 +0100

    (tests) Create Group from dashboard We add a test to create a group using the different charts available (OS, Antivirus, Machine online/offline)

--- test_00001_dashboard.py
+++ test_00001_dashboard.py
@@ -1,8 +1,9 @@
 from playwright.sync_api import  expect, Page
-from common import medulla_connect
+from common import medulla_connect, sqlcheck
 
 import configparser
 import os
+import re
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 Config = configparser.ConfigParser()
@@ -28,4 +29,82 @@ def test_dashboard_creategroup(page: Page) -> None:
     page.click("//html/body/div/div[4]/div/div[3]/div[1]/div[2]/div[2]/ul/li/a")
     expect(page).to_have_url(test_server + "/mmc/main.php?module=base&submod=groups&action=add")
 
+def test_create_groupe_by_dashboard_os_system(page: Page) -> None:
+    medulla_connect(page)
 
+    page.click("#os-graphs")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=display*"))
+
+    page.click("#namepresence1")
+    page.click(".edit a")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=computersgroupedit*"))
+
+    page.fill("//html/body/div/div[4]/div/form/table/tbody/tr[1]/td[3]/input", "Group Created by playwright By Dashboard OS System")
+
+    page.click(".btnPrimary[type='submit']")
+
+    result_on_server = sqlcheck("dyngroup", "SELECT count(*) from Groups WHERE name = 'Group Created by playwright By Dashboard OS System'")
+
+    assert result_on_server == 1
+
+    locator = page.locator(".alert")
+    expect(locator).to_have_class("alert alert-success")
+
+def test_create_groupe_by_dashboard_antivirus(page: Page) -> None:
+    medulla_connect(page)
+
+    locator = page.locator(".antivirus-graphsLabel0 a")
+    expect(locator).to_have_text(re.compile("OK:*"))
+
+    page.click(".antivirus-graphsLabel0 a")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=display*"))
+
+    page.click("#namepresence1")
+    page.click(".edit a")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=computersgroupedit*"))
+
+    page.fill("//html/body/div/div[4]/div/form/table/tbody/tr[1]/td[3]/input", "Group Created by playwright By Dashboard Antivirus")
+
+    page.click(".btnPrimary[type='submit']")
+
+    result_on_server = sqlcheck("dyngroup", "SELECT count(*) from Groups WHERE name = 'Group Created by playwright By Dashboard Antivirus'")
+
+    assert result_on_server == 1
+
+    locator = page.locator(".alert")
+    expect(locator).to_have_class("alert alert-success")
+
+def template_create_group_by_status(page: Page, selector, group_name) -> None:
+    """
+    It's a template to create a group of machines online or offline depending on the status available on the dashboard
+    Args:
+        selector: selector on which we want to click
+        group_name: name of the group you want to give
+    """
+    page.click(selector)
+    expect(page).to_have_url(re.compile(".*submod=computers&action=display*"))
+
+    page.click("#namepresence1")
+    page.click(".edit a")
+    expect(page).to_have_url(re.compile(".*submod=computers&action=computersgroupedit*"))
+
+    page.fill("//html/body/div/div[4]/div/form/table/tbody/tr[1]/td[3]/input", "Group Created by playwright By Dashboard " + group_name)
+
+    page.click(".btnPrimary[type='submit']")
+
+    result_on_server = sqlcheck("dyngroup", "SELECT count(*) from Groups WHERE name = 'Group Created by playwright By Dashboard " + group_name + "'")
+
+    assert result_on_server == 1
+
+    locator = page.locator(".alert")
+    expect(locator).to_have_class("alert alert-success")
+
+def test_create_groupe_by_dashboard_machine_online(page: Page) -> None:
+    medulla_connect(page)
+
+    status = sqlcheck("xmppmaster", "SELECT status FROM uptime_machine WHERE hostname = 'ath-win-1' ORDER BY id DESC LIMIT 1")
+
+    if(status == 1):
+        template_create_group_by_status(page, ".computersonline-graphLabel0 a", "Machine Online")
+    else:
+        template_create_group_by_status(page, ".computersonline-graphLabel1 a", "Machine Offline")
