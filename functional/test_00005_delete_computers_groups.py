1b70f124121c5cbb6cf164086fc04ea4bdc68ea0
Nicolas Lécureuil <neoclust@mageia.org>
2024-06-13 14:22:55 +0200

    (test_00005_delete_computers_groups) Fix deleting groups

--- functional/test_00005_delete_computers_groups.py
+++ functional/test_00005_delete_computers_groups.py
@@ -1,5 +1,8 @@
+from playwright.sync_api import  expect, Page
 from common import sqlcheck
 
-sqlcheck("dyngroup", "DELETE FROM Results")
-sqlcheck('dyngroup', "DELETE FROM ShareGroup")
-sqlcheck('dyngroup', "DELETE FROM Groups")
+def test_delete_computers_group(page: Page) -> None:
+    print("OK")
+    sqlcheck("dyngroup", "DELETE FROM Results")
+    sqlcheck('dyngroup', "DELETE FROM ShareGroup")
+    sqlcheck('dyngroup', "DELETE FROM Groups")
